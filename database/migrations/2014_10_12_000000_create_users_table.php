9343d963a37834e6cc0ffda22409e1f46c576b70
Rafael Ramos <161553@upf.br>
2019-05-17 16:05:17 -0300

    * Criação das Models Balance & Historic * Criação das Migrations Balance & Historic * Alteração da Migration User

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            //imagem vai armazernar um string do caminho e pode conter apenas 100 caracteres
+            $table->string('image', 100)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
